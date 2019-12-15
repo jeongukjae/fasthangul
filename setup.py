556c8109e67dfec6f47b83000457b83fc0b26898
jeongukjae <JeongUkJae@gmail.com>
2019-12-15 20:38:18 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,8 @@
 from distutils.core import setup, Extension
 
-fasthangul = Extension(
-    "fasthangul",
-    sources=["fasthangul/PythonExtension.cc"],
-    extra_compile_args=["-std=c++17"],
-    language="c++",
-)
 fasthangul_jamo = Extension(
     "fasthangul.jamo",
     sources=["fasthangul/jamo.cc"],
-    depends=["fasthangul/jamo.hh"],
     extra_compile_args=["-std=c++17"],
     language="c++",
 )
@@ -17,7 +10,8 @@ fasthangul_jamo = Extension(
 setup(
     name="fasthangul",
     version="1.0.0a0",
-    ext_modules=[fasthangul, fasthangul_jamo],
+    packages=['fasthangul'],
+    ext_modules=[fasthangul_jamo],
     url="https://github.com/jeongukjae/fasthangul",
     author="Jeong Ukjae",
     author_email="jeongukjae@gmail.com",
