f65f6b839a3bd41c576603e8795bab20f19ac5a7
jeongukjae <JeongUkJae@gmail.com>
2019-12-15 20:42:26 +0900

    Update bench_fasthangul.py

--- benchmark/bench_fasthangul.py
+++ benchmark/bench_fasthangul.py
@@ -1,17 +1,24 @@
-from fasthangul import jamo
+from fasthangul import decompose
 import random
 import string
 import time
 
-N_SENTENCES = 1000
-MIN_WORD_LENGTH = 30
+N_SENTENCES = 10000
+MIN_WORD_LENGTH = 500
 MAX_WORD_LENGTH = 1000
 
-letters = string.ascii_letters + "".join(map(chr, range(ord('가'), ord('힣') + 1))) + "          "
+letters = (
+    string.ascii_letters
+    + "".join(map(chr, range(ord("가"), ord("힣") + 1)))
+    + "          "
+)
 
 
 def get_random_string():
-    return "".join(random.sample(letters, random.randint(MIN_WORD_LENGTH, MAX_WORD_LENGTH)))
+    return "".join(
+        random.sample(letters, random.randint(MIN_WORD_LENGTH, MAX_WORD_LENGTH))
+    )
+
 
 def main():
     sentences = set(get_random_string() for _ in range(N_SENTENCES))
@@ -19,16 +26,15 @@ def main():
         sentences.add(get_random_string())
 
     sentences = list(sentences)
-    decomposed = []
-    print(f"average length: {len(''.join(sentences)) / N_SENTENCES}")
+    print(
+        f"average length: {len(''.join(sentences)) / N_SENTENCES}, "
+        f"n sentences: {N_SENTENCES}"
+    )
 
     s = time.time()
-    # decomposed = [jamo.decompose(sentence) for sentence in sentences]
-    # jamo.decompose("".join(sentences))
+    decompose("".join(sentences))
     print(f"decomposition time: {time.time() - s}")
 
-    # s = time.time()
-    # print(f"composing time: {time.time() - s}")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
