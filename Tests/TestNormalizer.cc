d27fc205fe2c955d1438d4072ad1db84e17aa2b7
jeongukjae <JeongUkJae@gmail.com>
2019-12-17 12:26:54 +0900

    update test code for is control char function

--- Tests/TestNormalizer.cc
+++ Tests/TestNormalizer.cc
@@ -4,10 +4,16 @@
 using namespace fasthangul::normalizer;
 
 TEST(normalizer, testIsControlChar) {
+  ASSERT_TRUE(isControlChar(L'\x00'));
+  ASSERT_TRUE(isControlChar(L'\x10'));
+  ASSERT_TRUE(isControlChar(L'\x1f'));
+  ASSERT_TRUE(isControlChar(L'\x7f')); // DEL
+
   ASSERT_FALSE(isControlChar('\n'));
   ASSERT_FALSE(isControlChar('\t'));
   ASSERT_FALSE(isControlChar('\r'));
   ASSERT_FALSE(isControlChar(L'\n'));
   ASSERT_FALSE(isControlChar(L'\t'));
   ASSERT_FALSE(isControlChar(L'\r'));
+  ASSERT_FALSE(isControlChar(L'ㄱ'));
 }
