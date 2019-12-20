442bedf3d29b3db0ff79eb6ffe163f34fe881b62
JeongUkJae <jeongukjae@gmail.com>
2019-12-20 22:01:11 +0900

    update vocab test

--- Tests/TestVocab.cc
+++ Tests/TestVocab.cc
@@ -4,7 +4,7 @@
 
 using namespace fasthangul::vocab;
 
-TEST(vocab, initialize) {
+TEST(vocab, testVocab) {
   std::vector<std::wstring> words{L"<PAD>", L"<UNK>", L"나", L"너", L"우리"};
   Vocab vocab{words};
 
@@ -14,6 +14,13 @@ TEST(vocab, initialize) {
   ASSERT_EQ(vocab.find(L"너"), 3);
   ASSERT_EQ(vocab.find(L"우리"), 4);
   ASSERT_EQ(vocab.find(L"이거 없다"), 1);
+
+  ASSERT_TRUE(vocab.has(L"<PAD>"));
+  ASSERT_TRUE(vocab.has(L"<UNK>"));
+  ASSERT_TRUE(vocab.has(L"나"));
+  ASSERT_TRUE(vocab.has(L"너"));
+  ASSERT_TRUE(vocab.has(L"우리"));
+  ASSERT_FALSE(vocab.has(L"이거 없다"));
 
   ASSERT_STREQ(vocab.at(0).data(), L"<PAD>");
   ASSERT_STREQ(vocab.at(1).data(), L"<UNK>");
