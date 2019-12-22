068fc6e4ec6850ecdc054a82c7da1a2085d390bc
jeongukjae <JeongUkJae@gmail.com>
2019-12-22 18:06:42 +0900

    update install-mkl.sh

--- Scripts/install-mkl.sh
+++ Scripts/install-mkl.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 if [ "$(uname)" == "Darwin" ]; then
   echo "Install MKL On MacOS"
-  export MKL_LIB_FILE=mkl-2019.5-intel_281
-  export MKL_INC_FILE=mkl-include-2019.5-281
-  curl -SLO https://anaconda.org/intel/mkl/2019.5/download/osx-64/${MKL_LIB_FILE}.tar.bz2
-  curl -SLO https://anaconda.org/anaconda/mkl-include/2019.5/download/osx-64/${MKL_INC_FILE}.tar.bz2
-
-  mkdir ${MKL_LIB_FILE} ${MKL_INC_FILE}
-  tar -xvjf ${MKL_LIB_FILE}.tar.bz2 -C ${MKL_LIB_FILE}
-  tar -xvjf ${MKL_INC_FILE}.tar.bz2 -C ${MKL_INC_FILE}
-
-  mv ${MKL_LIB_FILE}/lib /opt/intel/mkl/lib
-  mv ${MKL_INC_FILE}/include /opt/intel/mkl/include
+  export PLATFORM=osx-64
 else
+  echo "Install MKL On Linux"
+  export PLATFORM=linux-64
 fi
+
+export MKL_LIB_FILE=mkl-2019.5-intel_281
+export MKL_INC_FILE=mkl-include-2019.5-281
+curl -SLO https://anaconda.org/intel/mkl/2019.5/download/${PLATFORM}/${MKL_LIB_FILE}.tar.bz2
+curl -SLO https://anaconda.org/anaconda/mkl-include/2019.5/download/${PLATFORM}/${MKL_INC_FILE}.tar.bz2
+
+mkdir ${MKL_LIB_FILE} ${MKL_INC_FILE}
+tar -xvjf ${MKL_LIB_FILE}.tar.bz2 -C ${MKL_LIB_FILE}
+tar -xvjf ${MKL_INC_FILE}.tar.bz2 -C ${MKL_INC_FILE}
+
+mv ${MKL_LIB_FILE}/lib /opt/intel/mkl/lib
+mv ${MKL_INC_FILE}/include /opt/intel/mkl/include
