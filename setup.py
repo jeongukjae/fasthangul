0a815e9b3a22aa388fdc40ae6da3a09d9a6e0be6
JeongUkJae <jeongukjae@gmail.com>
2020-05-14 23:33:35 +0900

    fix wrong info, directory

--- setup.py
+++ setup.py
@@ -27,9 +27,9 @@ setup(
         "Natural Language :: Korean",
         "Programming Language :: C++",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: Implementation :: CPython",
     ],
 )
